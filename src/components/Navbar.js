b3a2290c427993381014bcc101f9e907ffb9974a
ajaykrshna <ajaykfan@gmail.com>
2023-07-15 17:11:51 +0530

    changed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <div className="bottomnavbar">
-            <Link to='/' className="links">
-                <ion-icon name="home" id='bottomnavbariconshome'></ion-icon>
-            </Link>
-            <Link to='/discover' className="links">
-                <ion-icon name="compass-outline" id='bottomnavbaricons'></ion-icon>
-            </Link>
-            <Link to='/test' className="links">
-                <ion-icon name="bar-chart-outline" id='bottomnavbaricons'></ion-icon>
-            </Link>
-            <Link to='/account' className="links">
-                <ion-icon name="person-outline" id='bottomnavbariconshome'></ion-icon>
-            </Link>
+            <NavLink to='/' className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "navlinks" : "pending"
+            }>
+                <ion-icon name="home" id='bottomnavbaricons'></ion-icon>
+            </NavLink>
+            <NavLink to='/discover' className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "navlinks" : "pending"
+            }>
+                <ion-icon name="compass" id='bottomnavbaricons'></ion-icon>
+            </NavLink>
+            <NavLink to='/test' className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "navlinks" : "pending"
+            }>
+                <ion-icon name="bar-chart" id='bottomnavbaricons'></ion-icon>
+            </NavLink>
+            <NavLink to='/account' className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "navlinks" : "pending"
+            }>
+                <ion-icon name="person" id='bottomnavbaricons'></ion-icon>
+            </NavLink>
         </div>
     )
 }
