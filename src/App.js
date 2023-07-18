e6d9ba46c995fabb19a7ed4b8768b64ab918e95d
ajaykrshna <ajaykfan@gmail.com>
2023-07-18 17:28:10 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import Accounts from "./components/Accounts";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Yahoodataid from "./components/Yahoodataid";
 import DesktopPage from "./components/DesktopPage";
+import Welcomepage from "./components/Welcomepage";
+import Name from "./components/Signup/Name";
+import Email from "./components/Signup/Email";
+import PinUp from "./components/Signup/Pin";
+import PinIn from "./components/Signin/Pin";
+import Success from "./components/Signup/Success";
+import Signin from "./components/Signin/Signin";
 
 function App() {
 
@@ -27,7 +34,7 @@ function App() {
       window.removeEventListener('resize', handleWindowResize);
     };
   }, []);
-  
+
   return (
     <>
       {(windowSize[0] > 800) ? <DesktopPage /> :
@@ -38,7 +45,7 @@ function App() {
               <Route path="/spotify" element={<Stockpage />} />
               <Route path="/discover" element={<Discover />} />
               <Route path="/account" element={<Accounts />} />
-              <Route path="/test" element={<Yahoodataid/>} />
+              <Route path="/test" element={<PinIn />} />
             </Routes>
           </Router>
         </div>}
